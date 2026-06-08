c60b711e00d8626f795a648920c7cf08795f0317
Barla Fam <nd4zmoe@gmail.com>
2026-06-08 22:10:13 +0700

    Update app version to 6.6 in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '6.0';
+const APP_VERSION = '6.6';
 const CACHE_PREFIX = 'uang-fambarla-';
 const CACHE_STATIC = CACHE_PREFIX + 'static-v' + APP_VERSION;
 const CACHE_DYNAMIC = CACHE_PREFIX + 'dynamic-v' + APP_VERSION;
